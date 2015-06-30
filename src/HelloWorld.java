144f6de18181553f62cd91f49a108d75e2c9e66a
nutanvdsi <nutan.prakash.x.choubey@verizon.com>
2015-07-01 01:57:34 +0530

    Hello2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld extends HttpServlet {
   public void init() throws ServletException
   {
       // Do required initialization
-      message = "Hello World";
+      message = "Hello World2";
   }
 
   public void doGet(HttpServletRequest request,
